2251f5407e5f18689be159dd751028833b7c0e8d
catapillie <espritox@gmail.com>
2022-04-29 00:56:27 +0200

    Add mod settings and Dialog files

--- Code/ShatteringStrawberriesModuleSettings.cs
+++ Code/ShatteringStrawberriesModuleSettings.cs
@@ -1,5 +1,16 @@
 namespace Celeste.Mod.ShatteringStrawberries {
+    [SettingName("modoptions_ShatteringStrawberries")]
     public class ShatteringStrawberriesModuleSettings : EverestModuleSettings {
+        [SettingName("modoptions_ShatteringStrawberries_Enabled")]
+        [SettingSubText("modoptions_ShatteringStrawberries_Enabled_desc")]
         public bool Enabled { get; set; } = true;
+
+        [SettingName("modoptions_ShatteringStrawberries_Shards")]
+        [SettingSubText("modoptions_ShatteringStrawberries_Shards_desc")]
+        public bool Shards { get; set; } = true;
+
+        [SettingName("modoptions_ShatteringStrawberries_Juice")]
+        [SettingSubText("modoptions_ShatteringStrawberries_Juice_desc")]
+        public bool Juice { get; set; } = true;
     }
 }
