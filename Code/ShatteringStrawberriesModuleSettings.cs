5c90812ff438ac63658270f187c436a4fe85e39a
catapillie <espritox@gmail.com>
2022-04-29 01:55:13 +0200

    Add Juice setting warning + dialog tweaks

--- Code/ShatteringStrawberriesModuleSettings.cs
+++ Code/ShatteringStrawberriesModuleSettings.cs
@@ -1,3 +1,8 @@
+using Celeste.Mod.ShatteringStrawberries.UI;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
 namespace Celeste.Mod.ShatteringStrawberries {
     [SettingName("modoptions_ShatteringStrawberries")]
     public class ShatteringStrawberriesModuleSettings : EverestModuleSettings {
@@ -11,6 +16,16 @@ namespace Celeste.Mod.ShatteringStrawberries {
 
         [SettingName("modoptions_ShatteringStrawberries_Juice")]
         [SettingSubText("modoptions_ShatteringStrawberries_Juice_desc")]
-        public bool Juice { get; set; } = true;
+        public bool Juice { get; set; } = false;
+        public void CreateJuiceEntry(TextMenu menu, bool _) {
+            var item = new TextMenu.OnOff(Dialog.Clean("modoptions_ShatteringStrawberries_Juice"), Juice)
+                .Change(value => Juice = value);
+
+            menu.Add(item);
+
+            item.AddWarning(menu, "         " + Dialog.Clean("modoptions_ShatteringStrawberries_Juice_warn_b"), icon: false);
+            item.AddWarning(menu, "    " + Dialog.Clean("modoptions_ShatteringStrawberries_Juice_warn_a"), icon: true);
+            item.AddDescription(menu, Dialog.Clean("modoptions_ShatteringStrawberries_Juice_desc"));
+        }
     }
 }
---
+++ Code/UI/TextMenuExtWarning.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.ShatteringStrawberries.UI {
+    public static class TextMenuExtWarning {
+        public static TextMenu.Item AddWarning(this TextMenu.Item option, TextMenu containingMenu, string description, bool icon) {
+            TextMenuExt.EaseInSubHeaderExt descriptionText = new(description, false, containingMenu, icon ? "areas/new-yellow" : null) {
+                TextColor = Color.Firebrick,
+                HeightExtra = 0f
+            };
+
+            List<TextMenu.Item> items = containingMenu.GetItems();
+            if (items.Contains(option))
+                TextMenuExt.Insert(containingMenu, items.IndexOf(option) + 1, descriptionText);
+
+            option.OnEnter = (Action)Delegate.Combine(option.OnEnter, () => { descriptionText.FadeVisible = true; });
+            option.OnLeave = (Action)Delegate.Combine(option.OnLeave, () => { descriptionText.FadeVisible = false; });
+
+            return option;
+        }
+    }
+}
